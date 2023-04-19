54577b3c17f40cfedae2efd699d9d3f7de840f62
mirzakomic <komic.mirza@gmail.com>
2023-04-19 16:27:24 +0200

    updated project, added new section and js animations

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,16 @@
-let btn = document.querySelector(".btn");
+const btns = document.querySelectorAll(".btn");
 
-btn.addEventListener("mousemove", (e) => {
-  const { width, height } = e.currentTarget.getBoundingClientRect()
-  btn.style.transform = `translate3d(${(e.offsetX - (width / 2)) / 2}px, ${(e.offsetY - (height / 2)) / 2}px, 0px)`;
+btns.forEach(btn => {
+  btn.addEventListener("mousemove", (e) => {
+    const { width, height } = e.currentTarget.getBoundingClientRect()
+    btn.style.transform = `translate3d(${(e.offsetX - (width / 2)) / 2}px, ${(e.offsetY - (height / 2)) / 2}px, 0px)`;
+  });
 
+  btn.addEventListener("mouseout", (e) => {
+    btn.style.transform = "none";
+  });
 });
 
-btn.addEventListener("mouseout", (e) => {
-  btn.style.transform = "none";
-});
 
 let btnplay = document.querySelector(".btn_special");
 
@@ -21,3 +23,65 @@ btnplay.addEventListener("mousemove", (e) => {
 btnplay.addEventListener("mouseout", (e) => {
   btnplay.style.transform = "none";
 });
+
+
+const costumeLink = document.querySelector('.costume');
+const actingLink = document.querySelector('.acting');
+const modellingLink = document.querySelector('.modelling');
+const artistLink = document.querySelector('.artist');
+const btmleftImgs = document.querySelectorAll('.btmleft img');
+const toprightImgs = document.querySelectorAll('.topright img');
+
+function changeOpacity(imgClass) {
+  btmleftImgs.forEach((img) => {
+    if (img.classList.contains(imgClass)) {
+      img.style.opacity = 1;
+    } else {
+      img.style.opacity = 0;
+    }
+  });
+  
+  toprightImgs.forEach((img) => {
+    if (img.classList.contains(imgClass)) {
+      img.style.opacity = 1;
+    } else {
+      img.style.opacity = 0;
+    }
+  });
+}
+
+costumeLink.addEventListener('mouseover', () => {
+  changeOpacity('img-cover');
+});
+
+actingLink.addEventListener('mouseover', () => {
+  changeOpacity('img-cover2');
+});
+
+modellingLink.addEventListener('mouseover', () => {
+  changeOpacity('img-cover3');
+});
+
+artistLink.addEventListener('mouseover', () => {
+  changeOpacity('img-cover4');
+});
+
+
+const midmenuLinks = document.querySelectorAll(".midmenu a");
+const imgCovers = document.querySelectorAll(".img-cover, .img-cover2, .img-cover3, .img-cover4");
+
+midmenuLinks.forEach(link => {
+  link.addEventListener("mousemove", (e) => {
+    const { width, height } = e.currentTarget.getBoundingClientRect();
+    imgCovers.forEach(imgCover => {
+      imgCover.style.transform = `translate3d(${(e.offsetX - (width / 2)) / 2}px, ${(e.offsetY - (height / 2)) / 2}px, 0px)`;
+    });
+  });
+
+  link.addEventListener("mouseout", (e) => {
+    imgCovers.forEach(imgCover => {
+      imgCover.style.transform = "none";
+    });
+  });
+});
+
